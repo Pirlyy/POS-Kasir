03c4b15e67f9f20ae715ff39f797e9dc846d44eb
areljagad <arelanakganteng@gmail.com>
2026-01-27 17:54:24 +0700

    struck

--- routes/web.php
+++ routes/web.php
@@ -97,6 +97,8 @@ Route::middleware('auth')->group(function () {
         ->group(function () {
             Route::get('/', 'index')->name('index');
             Route::post('/', 'store')->name('store');
+            // ✅ PRINT STRUK
+Route::get('/{id}/print', 'print')->name('print');
         });
 
     // =====================
@@ -121,6 +123,8 @@ Route::middleware('auth')->group(function () {
             ->group(function () {
                 Route::get('/laporan', 'laporan')->name('laporan');
                 // ❌ TIDAK ADA ROUTE DETAIL
+
+
             });
     });
 });
